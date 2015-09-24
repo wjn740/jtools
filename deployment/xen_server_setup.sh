3b6b7e859417d3965d81da70dc349c479386e118
James Wang <jnwang@suse.com>
2015-09-24 15:15:10 +0800

    some of things tidy later

---
+++ deployment/xen_server_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+for number in 211 114
+do
+    echo 147.2.207.$number:
+    sshpass -p ${password} ssh root@147.2.207.${number} "yast virtualization"
+    sshpass -p ${password} ssh root@147.2.207.${number} "yast kdump"
+done
+
+for number in 54
+do
+    echo 10.162.2.$number:
+    sshpass -p ${password} ssh root@10.162.2.${number} "yast virtualization"
+    sshpass -p ${password} ssh root@10.162.2.${number} "yast kdump"
+done
+
+
+wait
--- monitor/ssh-copy-id.sh
+++ monitor/ssh-copy-id.sh
@@ -3,6 +3,11 @@
 for number in 210 211 209 163 114
 do
     echo 147.2.207.$number:
-    sshpass -p 'susetesting' ssh-copy-id root@147.2.207.${number}
+    sshpass -p '${password}' ssh-copy-id root@147.2.207.${number}
 done
 
+for number in 53 54
+do
+    echo 10.162.2.$number:
+    sshpass -p '${password}' ssh-copy-id root@10.162.2.${number}
+done
