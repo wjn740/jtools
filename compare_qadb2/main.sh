38c3c2de94e3d4aee1da951825f1c70026d06409
James Wang <jnwang@suse.com>
2015-10-26 14:05:54 +0800

    Add some files about comparing tools plugin

--- compare_qadb2/main.sh
+++ compare_qadb2/main.sh
@@ -103,7 +103,7 @@ speed_formula()
 }
 iozone_formula()
 {
-    awk '{printf "%s/%s/%-15s\t %-10s\t %-10s\t %+0.5f\t%s,%s\t%s,%s\n",$1,$2,$3,$4,$5,$5/$4*100-100,$4,$5,$6,$7}'
+    awk '{printf "%s/%s/%-15s\t %-10s\t %-10s\t %+0.5f\t%s,%s\t%s,%s\n",$1,$2,$3,$4,$5,$5/$4*100-100,$6,$7,$8,$9}'
     #speed_formula
 }
 kernbench_formula()
