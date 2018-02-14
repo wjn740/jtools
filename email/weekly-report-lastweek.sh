e2bec2a041cb1ad6d64247f0000bc58ff470097f
James Wang <jnwang@suse.com>
2018-02-14 14:04:10 +0800

    update e-mail script for weekly-report-lastweek.sh

--- email/weekly-report-lastweek.sh
+++ email/weekly-report-lastweek.sh
@@ -4,4 +4,5 @@
 
 week=`date "+%V"`
 week=$(($week - 1))
-mailx -v -A suse -s "[qa-reports] Workreport of week ${week}, 2016" qa-reports@suse.de
+year=`date "+%G"`
+mailx -v -A suse -s "[qa-reports] Workreport of week ${week}, ${year}" qa-reports@suse.de
