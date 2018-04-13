fa2174f928f8f3506c0d90f6826b23ae1555702a
James Wang <jnwang@suse.com>
2018-04-13 12:10:20 +0800

    Add support SLE15 addon

--- deployment/installer.sh
+++ deployment/installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-/usr/share/qa/tools/install.pl -o "console=ttyS0,115200n8 ssh=1 sshpassword=${password}" -p "$@"
+/usr/share/qa/tools/install.pl -o "addon=$2 console=ttyS0,115200n8 ssh=1 sshpassword=${password}" -p "$1"
 sed -i 's/auto.*\.xml //g' /boot/grub2/grub.cfg
