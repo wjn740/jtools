d71f6b0432276863bcf508e1b550768ee5117593
James Wang <jnwang@suse.com>
2018-07-16 13:50:19 +0800

    [virt-io-test] SLE12-SP4 VM testing tool
    Add a set of testing tools for VM io testing.

---
+++ virt-tools/SLE12-SP4/virt-io-test/guestfish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -
+set -e
+
+guestname="$1"
+
+virsh dumpxml ${guestname} >${guestname}.vm.xml
+
+guestfish -d "${guestname}" <<EOF
+	run
+	mount /dev/sda2 /
+	copy-in install_automation_sles12_sp4_vm.sh /root/
+	mkdir /root/qaset/
+	copy-in list /root/qaset/
+	copy-in config /root/qaset/
+	copy-in parted-vm.sh /root/
+	copy-in ${guestname}.vm.xml /root
+EOF
